9d2b93cee52e805fd4b68c93ffd1bc27b8dd924d
Grant Shotwell <gcshotwell@gmail.com>
2020-08-04 16:31:35 -0400

    Initial marching cubes

--- Assets/MarchingCubes.cs
+++ Assets/MarchingCubes.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(TerrainValues))]
+[RequireComponent(typeof(MeshFilter))]
 public class MarchingCubes : MonoBehaviour {
 
     public ComputeShader shader;
-    Mesh mesh;
 
+    Mesh mesh;
     Vector3[] verticies;
     int[] trianges;
     
@@ -15,6 +16,7 @@ public class MarchingCubes : MonoBehaviour {
 
 
     void Start() {
+        terrain = GetComponent<TerrainValues>();
         mesh = GetComponent<MeshFilter>().mesh;
         GenerateMesh();
         ApplyMesh();
@@ -22,11 +24,26 @@ public class MarchingCubes : MonoBehaviour {
 
     void GenerateMesh() {
 
-        ComputeBuffer points, triangles;
-        shader.SetBuffer(0, "points", points);
+        CreateBuffers(out ComputeBuffer values, out ComputeBuffer triangles);
+
+        shader.SetBuffer(0, "values", values);
         shader.SetBuffer(0, "triangles", triangles);
         shader.SetInts("size", terrain.size.x, terrain.size.y, terrain.size.z);
+        shader.SetFloat("cutoff", terrain.cutoff);
 
+        shader.Dispatch(0, terrain.size.x, terrain.size.y, terrain.size.z);
+
+
+
+    }
+
+    void CreateBuffers(out ComputeBuffer values, out ComputeBuffer triangles) {
+
+        var size = terrain.size;
+        
+        values = new ComputeBuffer(size.x * size.y * size.z, sizeof(float));
+        triangles = new ComputeBuffer(0, sizeof(float) * 3 * 3);
+        values.SetData(terrain.values);
 
     }
 
