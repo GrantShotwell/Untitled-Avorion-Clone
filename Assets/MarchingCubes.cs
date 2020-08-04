f72d4fb59562cd3da360025d799fa78d91e67088
Grant Shotwell <gcshotwell@gmail.com>
2020-08-04 15:03:52 -0400

    Initial commit: Part 2

---
+++ Assets/MarchingCubes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TerrainValues))]
+public class MarchingCubes : MonoBehaviour {
+
+    public ComputeShader shader;
+    Mesh mesh;
+
+    Vector3[] verticies;
+    int[] trianges;
+    
+    TerrainValues terrain;
+
+
+    void Start() {
+        mesh = GetComponent<MeshFilter>().mesh;
+        GenerateMesh();
+        ApplyMesh();
+    }
+
+    void GenerateMesh() {
+
+        ComputeBuffer points, triangles;
+        shader.SetBuffer(0, "points", points);
+        shader.SetBuffer(0, "triangles", triangles);
+        shader.SetInts("size", terrain.size.x, terrain.size.y, terrain.size.z);
+
+
+    }
+
+    void ApplyMesh() {
+
+        mesh.Clear();
+        mesh.vertices = verticies;
+        mesh.triangles = trianges;
+        mesh.RecalculateNormals();
+
+    }
+
+}
---
+++ Assets/Scripts/TerrainValues.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainValues : MonoBehaviour {
+
+    [HideInInspector]
+    public float[,,] values;
+
+    [Range(0.00f, 1.00f)]
+    public float cutoff = 0.10f;
+
+    public Vector3Int size => new Vector3Int(values.GetLength(0), values.GetLength(1), values.GetLength(2));
+
+    void Start() {
+        values = new float[8, 8, 8];
+        GenerateValues();
+    }
+
+    void GenerateValues() {
+
+        int x = values.GetLength(0), y = values.GetLength(1), z = values.GetLength(2);
+        for(int ix = 0; ix < x; ix++) {
+            for(int iy = 0; iy < y; iy++) {
+                for(int iz = 0; iz < z; iz++) {
+
+                    float value = (float)NoiseS3D.Noise(ix, iy, iz);
+                    values[ix, iy, iz] = (value + 1) / 2;
+
+                }
+            }
+        }
+
+    }
+
+    void OnDrawGizmosSelected() {
+
+        if(values == null) return;
+
+        int x = values.GetLength(0), y = values.GetLength(1), z = values.GetLength(2);
+        for(int ix = 0; ix < x; ix++) {
+            for(int iy = 0; iy < y; iy++) {
+                for(int iz = 0; iz < z; iz++) {
+
+                    Vector3 position = new Vector3(ix, iy, iz);
+                    float value = values[ix, iy, iz];
+                    Color color = new Color { r = value, g = value, b = value, a = 1.0f };
+
+                    if(value <= cutoff) continue;
+
+                    Gizmos.color = color;
+                    Gizmos.DrawSphere(position, 0.1f);
+
+                }
+            }
+        }
+
+    }
+
+}
+
+
